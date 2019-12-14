f4daaa8679dcf9f971c07f800d9bd3739f360e0b
4meta5 <asinghchrony@protonmail.com>
2019-12-14 13:32:10 -0500

    method chaining -- next going to make less manual

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,16 +10,50 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = &ast.ident;
     let bname = format!("{}Builder", name);
     let bident = syn::Ident::new(&bname, name.span());
-    let expanded = quote! {
-        struct #bident{
+    let expected = quote! {
+        pub struct #bident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+
+        }
+        impl #bident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    executable: self.executable.clone().ok_or("executable not set")?,
+                    args: self.args.clone().ok_or("executable not set")?,
+                    env: self.env.clone().ok_or("executable not set")?,
+                    current_dir: self.current_dir.clone().ok_or("executable not set")?,
+                })
+            }
         }
         impl #name {
             fn builder() -> #bident {
                 #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
                 }
             }
         }
     };
-
-    expanded.into()
+    expected.into()
 }
