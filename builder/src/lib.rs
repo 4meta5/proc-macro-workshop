ed5192bcae85a9fadc4653862f0b4d44623ffcc4
4meta5 <asinghchrony@protonmail.com>
2019-12-15 15:46:08 -0500

    stuck on 7 with duplicate method generation; need to resolve name conflicts

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,9 +4,9 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-fn ty_is_option(ty: &syn::Type) -> Option<&syn::Type> {
+fn get_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
     if let syn::Type::Path(ref p) = ty {
-        if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper {
             return None;
         }
         if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
@@ -23,7 +23,7 @@ fn ty_is_option(ty: &syn::Type) -> Option<&syn::Type> {
     None
 }
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
@@ -41,7 +41,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let optionized = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if ty_is_option(ty).is_some() {
+        if get_inner_type("Option", ty).is_some() {
             quote! { #name: #ty }
         } else {
             quote! { #name: std::option::Option<#ty> }
@@ -50,7 +50,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if let Some(inner_ty) = ty_is_option(ty) {
+        if let Some(inner_ty) = get_inner_type("Option", ty) {
             quote! {
                 pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
                     self.#name = Some(#name);
@@ -66,9 +66,51 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     });
+    let extend_methods = fields.iter().filter_map(|f| {
+        let name = &f.ident;
+        for attr in &f.attrs {
+            if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "builder" {
+                if let Some(proc_macro2::TokenTree::Group(g)) = attr.tokens.clone().into_iter().next() {
+                    let mut tokens = g.stream().into_iter();
+                    match tokens.next().unwrap() {
+                        proc_macro2::TokenTree::Ident(ref i) => assert_eq!(i, "each"),
+                        tt => panic!("unexpected 'each', found {}", tt),
+                    }
+                    match tokens.next().unwrap() {
+                        proc_macro2::TokenTree::Punct(ref p) => assert_eq!(p.as_char(), '='),
+                        tt => panic!("expected '=', found {}", tt),
+                    }
+                    let arg = match tokens.next().unwrap() {
+                        proc_macro2::TokenTree::Literal(l) => l,
+                        tt => panic!("expected literal, found {}", tt),
+                    };
+                    match syn::Lit::new(arg) {
+                        syn::Lit::Str(s) => {
+                            let arg = syn::Ident::new(&s.value(), s.span());
+                            let inner_ty = get_inner_type("Vec", &f.ty).unwrap();
+                            return Some(
+                                quote!{ 
+                                    fn #arg(&mut self, #arg: #inner_ty) -> &mut Self {
+                                        if let Some(ref mut values) = self.#name {
+                                            values.push(#arg);
+                                        } else {
+                                            self.#name = Some(vec![#arg]);
+                                        }
+                                        self
+                                    }
+                                }
+                            );
+                        },
+                        lit => panic!("expected string, found {:?}", lit),
+                    }
+                }
+            }
+        }
+        None
+    });
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
-        if ty_is_option(&f.ty).is_some() {
+        if get_inner_type("Option", &f.ty).is_some() {
             quote! {
                 #name: self.#name.clone()
             }
@@ -90,6 +132,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
         impl #bident {
             #(#methods)*
+            #(#extend_methods)*
 
             pub fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
                 Ok(
